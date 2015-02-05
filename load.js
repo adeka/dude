a22f46d48b9cc142b7bb7fda6ba825e24872e8d6
Nikita <gramps.master@gmail.com>
2015-02-05 01:25:13 -0500

    door action

--- load.js
+++ load.js
@@ -8,11 +8,13 @@ var load_state = {
 	});
 
 	this.game.load.tilemap('house', 'level/house.json', null, Phaser.Tilemap.TILED_JSON);
+	this.game.load.tilemap('room1', 'level/room1.json', null, Phaser.Tilemap.TILED_JSON);
 	this.game.load.image('tilesetBig', 'assets/tilesetBig.png');
 
 	this.game.load.spritesheet('character', 'assets/character.png');
 
 	this.game.load.spritesheet('legs', 'assets/legs/legs.png', 10, 4, 12);
+	this.game.load.spritesheet('door', 'assets/door.png', 12, 22, 5);
 	this.game.load.spritesheet('torso', 'assets/torso/torso.png', 16, 6, 8);
 	this.game.load.image('headIdle', 'assets/head/idle.png');
 
--- play.js
+++ play.js
@@ -1,16 +1,55 @@
+var interact = false;
+
+function Door(game,x,y, group, name, props){
+	this.open = false;
+	this.sprite = game.add.sprite(x, y, 'door');
+	this.sprite.dest = {x:props.destX, y: props.destY};
+
+	this.sprite.name = name;
+	this.sprite.scale = {x:5,y:5};
+	this.sprite.smoothed = false;
+	this.sprite.animations.add('open',[0,1,2,3,4]);
+	game.physics.arcade.enable(this.sprite);
+	//this.sprite.body.enable = false;
+	this.sprite.body.allowGravity = false;
+	this.sprite.body.immovable = true;
+	//
+	group.add(this.sprite);
+}
+
+function atDoor(player, door){
+	if(interact){
+		console.log(door.name);
+		interact = false;
+		door.animations.play('open', 8, false);
+		var x = door.dest.x;
+		var game = this;
+		this.frozen = true;
+		setTimeout(function() {
+			game.load_level(door.name, parseInt(door.dest.x), parseInt(door.dest.y));
+		}, 600);
+	}
+}
+
 var play_state = {
 	preload: function() {
-		game.stage.setBackgroundColor(0x7EC0EE);
+		game.stage.setBackgroundColor(0x000000);
 	},
 
 	create: function() {
 		this.wallGroup = this.game.add.group();
+		this.doorGroup = this.game.add.group();
+		//this.doorGroup.enableBody = true;
+
+
 		this.game.physics.startSystem(Phaser.Physics.ARCADE)
 		this.game.physics.arcade.gravity.y = 200;
 		this.game.stage.smoothed = false;
 		this.velocity = 0;
 		this.running = false
 		this.attacking = false;
+
+		//new Door(this.game, 450, 500);
 
 
 		//add character sprite and animations
@@ -59,13 +98,15 @@ var play_state = {
 		this.character.animations.add('crouch', [5, 6, 7, 8, 9], 30, false);
 		*/
 		
-		this.load_level(1);
+		this.load_level("house", 400, 1000);
 
 		this.left = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
 		this.right = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
 		this.down = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
 		this.up = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
 		this.attack = this.game.input.keyboard.addKey(Phaser.Keyboard.E);
+		this.jumpKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
 
 		this.attack.onDown.add(function() {
 			this.attacking = true;
@@ -85,9 +126,11 @@ var play_state = {
 			this.crouch=true;
 		}, this);
 		this.up.onDown.add(function() {
+			interact=true;
+		}, this);
+		this.jumpKey.onDown.add(function() {
 			this.jump=true;
 		}, this);
-
 
 
 		this.right.onUp.add(function() {
@@ -96,7 +139,8 @@ var play_state = {
 		this.left.onUp.add(function() {
 			this.running=false;
 		}, this);
-		this.up.onUp.add(function() {this.jump=false;}, this);
+		this.up.onUp.add(function() {interact=false;}, this);
+		this.jumpKey.onUp.add(function() {this.jump=false;}, this);
 
 		this.torso.body.enable = false;
 		this.head.body.enable = false;
@@ -105,32 +149,29 @@ var play_state = {
 	},
 
 	update: function() { 
+		if(this.frozen){
+			this.running = false;
+			this.jump = false;
+			this.velocity = 0;
+		}
+		this.game.physics.arcade.overlap(this.character, this.doorGroup, atDoor, null, this);
 		this.game.physics.arcade.collide(this.character, this.layer);
+
 		if(this.punching && this.punching.isFinished){
 			this.attacking = false;
 			this.punching = null;
 		}
-		/*
-		this.game.physics.arcade.overlap(this.character, this.flag, 
-			function () {
-				this.load_level(this.level + 1);
-			}, null, this);
-		*/
-
-
 
 		if(this.velocity <= .1 && this.velocity >= -.1){
 			this.velocity = 0;
 		}
 
 		if (this.jump && this.character.body.onFloor()) {
-			this.character.body.velocity.y = -600;
+			this.character.body.velocity.y = -550;
 			//console.log("pokemon");
 		}
 
-		this.character.body.velocity.x = this.velocity * 150;
-
-
+		this.character.body.velocity.x = this.velocity * 170;
 
 		if(this.running){
 
@@ -162,118 +203,64 @@ var play_state = {
 			}
 			this.torso.animations.play('jump', 8, false);
 		}
-/*
-	if (this.run) {
-		//this.character.animations.play('walk');
-
-		if (this.character.body.onFloor() && this.character.body.velocity.x > 300)
-			this.character.body.velocity.x -= 2;
-		else if (this.character.body.onFloor())
-			this.character.body.velocity.x = 300;
-		else if (this.character.body.velocity.x < 450)
-			this.character.body.velocity.x += 5;
-	} else if (this.character.body.onFloor()) {
-		this.character.body.velocity.x *= 0.9;
-	}
-*/
-
-
-	if (this.character.body.x < 0 || this.character.body.x >= this.game.world.width - 50 || 
-		this.character.body.y < 0 || this.character.body.y >= this.game.world.height - 50) {
-		this.die();
-}
-
-	// this.jump = false;
-	// this.crouch = false;
-	// this.run = false;
-		//this.torso.position = {x: this.character.position.x, y: this.character.position.y - 30};
-		//this.head.position = {x: this.character.position.x, y: this.character.position.y - 90};
-
 
 },
 
-load_level: function(level) {
-	if (this.layer) this.layer.destroy();
 
 
-/*
-	if (level == 1)
-		text = "RIGHT to run";
-	else if (level == 2)
-		text = "UP to jump";
-	else if (level == 3)
-		text = "DOWN to crouch";
-	if (1 <= level && level <= 3) {
-		this.infos = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 100, 
-			text, {
-				font: '60px Arial',
-				fill: '#87E8D1',
-				align: 'center',
-			});
-		this.infos.anchor.setTo(0.5, 0.5);
+load_level: function(level, x, y) {
+	this.frozen = false;
+
+	this.wallGroup.removeAll()
+	this.doorGroup.removeAll()
+	//if (this.doorGroup) this.doorGroup.destroy();
+	if(level == "house"){
+		this.map = this.game.add.tilemap('house');
 	}
-	*/
-
-	this.map = this.game.add.tilemap('house');
+	else{
+		this.map = this.game.add.tilemap(level);
+	}
 	this.map.addTilesetImage('tilesetBig', 'tilesetBig');
-	//this.world.scale = {x:2, y: 2};
-	//this.map.setCollisionBetween(1,30);	
+
 	
 	this.layer2 = this.map.createLayer('walls',1024,1024,this.wallGroup);	
 	this.layer3 = this.map.createLayer('subDecals',1024,1024,this.wallGroup);	
-	this.layer3 = this.map.createLayer('decals',1024,1024,this.wallGroup);	
+	this.layer4 = this.map.createLayer('decals',1024,1024,this.wallGroup);	
+	
 	this.layer = this.map.createLayer('layer',1024,1024,this.wallGroup);	
 
 	this.layer.resizeWorld();
 	this.map.setCollisionBetween(0, 30, true, "layer", true );
 
-	//this.layer2 = this.map.createLayer('walls');	
-	//this.layer3 = this.map.createLayer('decals');	
-	
-	/*
-	this.top_stalactites = this.game.add.group();
-	this.top_stalactites.enableBody = true;
-	this.top_stalactites.physicsBodyType = Phaser.Physics.ARCADE;
-	this.bot_stalactites = this.game.add.group();
-	this.bot_stalactites.enableBody = true;
-	this.bot_stalactites.physicsBodyType = Phaser.Physics.ARCADE;
+	var result = this.findObjectsByType(this.map, 'objectsLayer');
+ 
+    result.forEach(function(element){
+ 		new Door(this.game, element.x, element.y, this.doorGroup, element.name, element.properties);
+    }, this);
 
-	this.map.createFromObjects('stalactites', 9, 'top_stalactite', 0, true, false, this.top_stalactites);
-	this.map.createFromObjects('stalactites', 10, 'bot_stalactite', 0, true, false, this.bot_stalactites);
-	this.top_stalactites.forEach(function(s) {
-		s.body.allowGravity = false;
-		s.body.immovable = true;
-	}, this);
-	this.bot_stalactites.forEach(function(s) {
-		s.body.allowGravity = false;
-		s.body.immovable = true;
-	}, this);
-	
-
-	this.flag = this.game.add.group();
-	this.flag.enableBody = true;
-	this.flag.physicsBodyType = Phaser.Physics.ARCADE;
-	this.map.createFromObjects('flag', 4, 'flag', 0, true, false, this.flag);
-	this.flag.forEach(function(f) {
-		f.body.allowGravity = false;
-		f.body.immovable = true;
-	}, this);
-*/
 
 	this.game.physics.enable([this.character, this.layer]);
 
 	this.character.body.velocity.x = 0;
 	this.character.body.velocity.y = 0;
-	this.character.position.setTo(450, 500);
+	this.character.position.setTo(x, y);
 	this.character.body.gravity.y = 500;
 
 	this.game.camera.follow(this.character);
-	//this.game.camera.setBoundsToWorld();
-	//this.game.camera.focusOnXY(this.character.position.x,this.character.position.x));
 
 	this.level = level;
 },
 
+//find objects in a Tiled layer that containt a property called "type" equal to a certain value
+ 
+  findObjectsByType: function(map, layerName) {
+    var result = [];
+    map.objects[layerName].forEach(function(element){
+        result.push(element);
+    });
+    return result;
+  },
+ 
 die : function() {
 	/*
 	this.character.anchor.setTo(0.5, 0.5);
